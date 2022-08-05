8274595ac4b1b09a99ddc9776de3ece0fe2306d3
HP <sachdevamohita5@gmail.com>
2022-08-05 23:54:13 +0530

    happy or sad

---
+++ src/com/bridgelap/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,20 @@
+package com.bridgelap.moodanalyser;
+
+public class MoodAnalyser {
+
+        public static void main(String[] args) {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String happyMood = moodAnalyser.analyseMood("I am in Happy Mood");
+            System.out.println("My mood is :"+happyMood);
+            String sadMood = moodAnalyser.analyseMood("I am in Sad Mood");
+            System.out.println("My Mood is :"+sadMood);
+        }
+        String analyseMood (String message){
+            if(message.toLowerCase().contains("sad"))
+                return "SAD";
+            else if (message.toLowerCase().contains("happy"))
+                return "HAPPY";
+            return message;
+        }
+    }
+
